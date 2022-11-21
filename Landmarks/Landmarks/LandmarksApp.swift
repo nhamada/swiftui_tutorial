758945885b191f47a672489af8a627a3e4d3edc6
Naohiro Hamada <nao.pinguinus@icloud.com>
2022-11-21 23:20:02 +0900

    Create Landmarks app project

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Naohiro Hamada on 2022/11/21.
+//
+
+import SwiftUI
+
+/*
+ * main attribute -> App entry point
+ */
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
