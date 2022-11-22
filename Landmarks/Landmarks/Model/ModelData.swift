974ce1de13ad61c59ab9d885b6f1d9d72b39fed2
Naohiro Hamada <nao.pinguinus@icloud.com>
2022-11-22 21:56:48 +0900

    Create landmark model & add resources

---
+++ Landmarks/Landmarks/Model/ModelData.swift
@@ -0,0 +1,31 @@
+//
+//  ModelData.swift
+//  Landmarks
+//
+//  Created by Naohiro Hamada on 2022/11/22.
+//
+
+import Foundation
+
+var landmarks: [Landmark] = load("landmarkData.json")
+
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+    
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil) else {
+        fatalError("Could not find \(filename) in main bundle.")
+    }
+    
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Could not load \(filename) from main bundle: \(error)")
+    }
+    
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Could not parse \(filename) as \(T.self): \(error)")
+    }
+}
